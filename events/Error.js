e211b57ba142d7b625c5276b33492253a667c50b
dem0nx309 <41202006+dem0nx309@users.noreply.github.com>
2023-07-29 16:50:02 +0200

    Eslint

--- events/Error.js
+++ events/Error.js
@@ -1,8 +1,8 @@
-const { Events } = require('discord.js');
+const { Events } = require("discord.js");
 
 module.exports = {
-	name: Events.Error,
-	async execute() {
-		console.error();
-	},
+    name: Events.Error,
+    async execute() {
+        console.error();
+    },
 };

40e16b2a0e9bdccd405ade6dd0074c363d6f52af
dem0nx309 <41202006+dem0nx309@users.noreply.github.com>
2023-07-29 16:40:59 +0200

    Eslint

--- commands/embed.js
+++ commands/embed.js
@@ -1,19 +1,19 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("embed")
-    .setDescription(
-      "Explication sur le raffraichissement des images présentes dans les embeds."
-    ),
+    data: new SlashCommandBuilder()
+        .setName("embed")
+        .setDescription(
+            "Explication sur le raffraichissement des images présentes dans les embeds."
+        ),
 
-  async execute(interaction) {
-    const embed2 = new EmbedBuilder()
-      .setAuthor({ name: "🖼️ Embed (image sur les liens)" })
-      .setColor("#196ffa")
-      .setDescription(
-        "Les images présentes dans les embeds de liens sont générés sur serveur dès le partage du lien en question. Suivant où le lien est partagé (Twitter, Discord..), l'image peut-être mise en cache et ne peut donc s'actualiser durant plusieurs heures ou plusieurs jours."
-      )
-      .setTimestamp();
-    await interaction.reply({ embeds: [embed2] });
-  },
+    async execute(interaction) {
+        const embed2 = new EmbedBuilder()
+            .setAuthor({ name: "🖼️ Embed (image sur les liens)" })
+            .setColor("#196ffa")
+            .setDescription(
+                "Les images présentes dans les embeds de liens sont générés sur serveur dès le partage du lien en question. Suivant où le lien est partagé (Twitter, Discord..), l'image peut-être mise en cache et ne peut donc s'actualiser durant plusieurs heures ou plusieurs jours."
+            )
+            .setTimestamp();
+        await interaction.reply({ embeds: [embed2] });
+    },
 };

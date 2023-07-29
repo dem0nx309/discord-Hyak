132cc4e310c1af897594cf890cf0717f6cd4b2d4
dem0nx309 <41202006+dem0nx309@users.noreply.github.com>
2023-07-29 16:50:26 +0200

    Eslint

--- events/Error.js
+++ events/Error.js
@@ -1,8 +1,9 @@
 const { Events } = require("discord.js");
 
 module.exports = {
-    name: Events.Error,
-    async execute() {
-        console.error();
+    name: Events.ClientReady,
+    once: true,
+    async execute(client) {
+        console.log(`Ready! Logged in as ${client.user.tag}`);
     },
 };

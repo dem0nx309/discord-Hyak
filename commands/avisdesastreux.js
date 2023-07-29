498dc1247c5ce425bc6609db41293d2933d908df
dem0nx309 <41202006+dem0nx309@users.noreply.github.com>
2023-07-29 16:44:05 +0200

    Eslint

--- commands/avisdesastreux.js
+++ commands/avisdesastreux.js
@@ -1,13 +1,13 @@
 const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require("discord.js");
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("avisdesastreux")
-    .setDescription("Un avis posté qui est désastreux ? Voici la solution !"),
-  async execute(interaction) {
-    const file = new AttachmentBuilder("https://i.imgur.com/NzAL3dG.mp4");
-    const embed4 = new EmbedBuilder()
-      .setAuthor({ name: "🚨 ALERTE AVIS DÉSASTREUX 🚨" })
-      .setColor("#fa2525");
-    await interaction.reply({ files: [file], embeds: [embed4] });
-  },
+    data: new SlashCommandBuilder()
+        .setName("avisdesastreux")
+        .setDescription("Un avis posté qui est désastreux ? Voici la solution !"),
+    async execute(interaction) {
+        const file = new AttachmentBuilder("https://i.imgur.com/NzAL3dG.mp4");
+        const embed4 = new EmbedBuilder()
+            .setAuthor({ name: "🚨 ALERTE AVIS DÉSASTREUX 🚨" })
+            .setColor("#fa2525");
+        await interaction.reply({ files: [file], embeds: [embed4] });
+    },
 };

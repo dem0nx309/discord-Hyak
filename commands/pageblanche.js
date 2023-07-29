4cdc51cb5fb45a8fdd9e157b59e9b688eb8aa490
dem0nx309 <41202006+dem0nx309@users.noreply.github.com>
2023-07-29 16:39:47 +0200

    Eslint

--- commands/pageblanche.js
+++ commands/pageblanche.js
@@ -1,35 +1,35 @@
 const { SlashCommandBuilder,EmbedBuilder} = require("discord.js");
 module.exports = {
-  data: new SlashCommandBuilder().setName("pageblanche").setDescription(
-    "Explication sur les pages blanche."
-  ),
-  async execute(interaction) {
-    const embed = new EmbedBuilder()
-      .setAuthor({ name: "📄 Page Blanche" })
-      .setColor("#196ffa")
-      .setDescription(
-        "Les pages blanches sur le site peuvent apparaître à la suite de nombreuses causes:"
-      )
-      .addFields(
-        {
-          name: "• Bloqueur de publicité (AdBlock)",
-          value: `Désactivez votre bloqueur de publicité (et soutenez Hyakanime).`,
-          inline: false,
-        },
-        {
-          name: "• Cache du site internet",
-          value:
+    data: new SlashCommandBuilder().setName("pageblanche").setDescription(
+        "Explication sur les pages blanche."
+    ),
+    async execute(interaction) {
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: "📄 Page Blanche" })
+            .setColor("#196ffa")
+            .setDescription(
+                "Les pages blanches sur le site peuvent apparaître à la suite de nombreuses causes:"
+            )
+            .addFields(
+                {
+                    name: "• Bloqueur de publicité (AdBlock)",
+                    value: "Désactivez votre bloqueur de publicité (et soutenez Hyakanime).",
+                    inline: false,
+                },
+                {
+                    name: "• Cache du site internet",
+                    value:
             "Pour actualiser le cache du site, rendez-vous sur https://hyakanime.fr et appuyez simultanément sur `CTRL + F5`",
-          inline: false,
-        },
-        {
-          name: "\u200b",
-          value:
+                    inline: false,
+                },
+                {
+                    name: "\u200b",
+                    value:
             "Si le souci persiste, n'hésitez pas à mentionner <@245604480278593537>",
-          inline: false,
-        }
-      )
-      .setTimestamp();
-    await interaction.reply({ embeds: [embed] });
-  },
+                    inline: false,
+                }
+            )
+            .setTimestamp();
+        await interaction.reply({ embeds: [embed] });
+    },
 };
